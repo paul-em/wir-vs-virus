218e651ea1af53b68c563993d52895c28a0cc637
Paul Em <paul3m@gmail.com>
2020-03-22 17:05:24 +0100

    adjust title

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'What can I do? | Wir vs Virus',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
