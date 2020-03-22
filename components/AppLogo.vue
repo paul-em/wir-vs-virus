b13fc952e95808d0994fbc3d52114e13d051d2b2
Paul Em <paul3m@gmail.com>
2020-03-22 15:47:04 +0100

    add app logo

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="flex flex-col justify-center px-8">
-    <h3>Corona Virus</h3>
-    <h1>
-      What can <span class="font-black font-serif">I</span> do?
-    </h1>
+    <img src="wcId_blau.jpg">
   </div>
 </template>
 
 <style>
-
+  img {
+    max-height: 100px;
+  }
 </style>
